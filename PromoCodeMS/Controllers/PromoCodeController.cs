f1cf5c5d7060ea47807d2a3e24f3e9bf209356b1
Ei Ei Nyein <eieinyein@myanmareasenet.com>
2023-01-30 08:08:47 +0630

    Add project files.

---
+++ PromoCodeMS/Controllers/PromoCodeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PromoCodeMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PromoCodeController : ControllerBase
+    {
+        [HttpGet]
+        //[ProducesResponseType(typeof(string), 200)]
+        public async Task<IActionResult> GetPromoCode()
+        {
+            Random generator = new Random();
+            String code = generator.Next(0, 1000000).ToString("D6") + new Guid().ToString().Replace("-0123456789", "").Substring(0, 5).ToUpper();
+            return Ok(code);
+        }
+    }
+}
